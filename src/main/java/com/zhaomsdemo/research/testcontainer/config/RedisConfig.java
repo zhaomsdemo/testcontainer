21a4874a6f4e6162741e53a3ff487ca31604e378
zhaomsdemo <zhaomsdemo@gmail.com>
2024-06-05 21:46:43 -0700

    Added Redis test container

---
+++ src/main/java/com/zhaomsdemo/research/testcontainer/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.zhaomsdemo.research.testcontainer.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new JavaTimeModule())
+                .registerModule(new Jdk8Module());
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        RedisTemplate<String, Object> redisTemplate = getRedisTemplate(redisConnectionFactory, objectMapper);
+
+        return redisTemplate;
+    }
+
+    private static RedisTemplate<String, Object> getRedisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(serializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
---
+++ src/test/java/com/zhaomsdemo/research/testcontainer/nosql/RedisTest.java
@@ -0,0 +1,51 @@
+package com.zhaomsdemo.research.testcontainer.nosql;
+
+import com.redis.testcontainers.RedisContainer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Testcontainers
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    static RedisContainer redis = new RedisContainer(DockerImageName.parse("redis:7.0-alpine3.20"))
+                .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    public static void registryProperties(DynamicPropertyRegistry registry) {
+        redis.start();
+        System.setProperty("spring.data.redis.host", redis.getHost());
+        System.setProperty("spring.data.redis.port", redis.getMappedPort(6379).toString());
+    }
+
+    @BeforeAll
+    public static void setup() {
+    }
+
+    @AfterAll
+    public static void teardown() {
+        redis.stop();
+    }
+
+    @Test
+    public void testRedisOperations() {
+        redisTemplate.opsForValue().set("name", "zjh");
+        String value = redisTemplate.opsForValue().get("name");
+        assertEquals("zjh", value);
+    }
+}
